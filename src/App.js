78512a0f008023e7a4c9fbb089b85a77b00c7925
Hamid Kamyab <hamidkamyab1377@gmail.com>
2021-06-25 13:35:54 +0430

    adding Components and Specific Styles For Two Bottom Component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import HomePage from "./components/home-page/homepage.component"
+import HomePage from "./pages/home-page/homepage.component"
 function App() {
   return (
     <div>
---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import MenuItem from "../menu-item/menu-item.component" 
+import './directory.styles.scss';
+import sections from "./directory.data"
+
+class Directory extends React.Component{
+    constructor(){
+        super()
+        
+        this.state = {
+            data: sections
+        }
+    }
+
+    render(){
+        return(
+            <div className="directory-menu">
+                {
+                    this.state.data.map( ({id, title, imageUrl, size}) => (
+                        <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>
+                    ))
+                }
+            </div>
+        )
+    }
+}
+
+export default Directory;
