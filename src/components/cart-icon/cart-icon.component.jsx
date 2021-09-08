e816a30a882a74fcab2fff7c4ed48612f70290c9
hamid Kamyab <hamidkamyab1377@gmail.com>
2021-09-08 15:22:57 +0430

    updating our application with reselect replace cart selector with memoize selector

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -2,16 +2,16 @@ import React from "react";
 
 import { connect } from "react-redux";
 import toggleHidden from "../../redux/cart/cart-action";    
-
+import { selectCartItemsCount } from "../../redux/cart/cart-selectors";
 import {ReactComponent as ShoppingLogo } from "./11.2 shopping-bag.svg";
 
 import './cart-icon.styles.scss';
 
-const CartIcon = ({toggleHidden, cartItems}) => {
+const CartIcon = ({toggleHidden, cartItemsCount}) => {
     return (
         <div className = "cart-icon" onClick={toggleHidden}>
             <ShoppingLogo className="shopping-icon" />
-            <span className = "item-count">{cartItems.length}</span>
+            <span className = "item-count">{cartItemsCount}</span>
         </div>
     )
 }
@@ -20,8 +20,13 @@ const mapDispatchToProps = dispatch => ({
     toggleHidden: () => dispatch(toggleHidden())
 });
 
-const mapStateToProps = ({cart: {cartItems}}) => ({
-    cartItems
+const mapStateToProps = (state) => ({
+    // cartItemsCount: cartItems.reduce((accumalatedQuantity, cartItem) => 
+    //     accumalatedQuantity + cartItem.quantity ,
+    //     0)
+    cartItemsCount: selectCartItemsCount(state)
 })
+//with commented method react re-render the cart Each time we Add To Cart.
+//with Reselect we Can Get Benefit of Memoization
 
 export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
