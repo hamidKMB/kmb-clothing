9725502b3239ff600951a49b44e392fa8fee3bea
hamid Kamyab <hamidkamyab1377@gmail.com>
2021-09-09 16:17:53 +0430

    adding the shop data to their own Reducer and sace in store

--- src/pages/shop-page/shoppage.component.jsx
+++ src/pages/shop-page/shoppage.component.jsx
@@ -1,29 +1,27 @@
 import React from "react"
-import SHOP_DATA from "./shop.data";
+
+import { connect } from "react-redux"
+import { createStructuredSelector } from "reselect";
+import { selectShopItems } from "../../redux/shop/shop-selectors";
+
 import CollectionPreview from "../../components/collection-preview/collection-preview.component";
-class ShopPage extends React.Component{
-    constructor(props){
-        super(props)
 
-        this.state={
-            collections: SHOP_DATA
-        }
-    }
-
-    render() {
-        const {collections} = this.state;
-        return(
-            <div className="shop-page">
-                {
-                    collections.map(({id, ...otherCollectionProps}) => {
-                        return (
-                            <CollectionPreview key={id} {...otherCollectionProps}/>
-                        )
-                    })
-                }
-            </div>
-        )
-    }
+const ShopPage = ({collection}) => {
+    return(
+        <div className="shop-page">
+            {
+                collection.map(({id, ...otherCollectionProps}) => {
+                    return (
+                        <CollectionPreview key={id} {...otherCollectionProps}/>
+                    )
+                })
+            }
+        </div>
+    )
 }
 
-export default ShopPage
+const mapStateToProps = createStructuredSelector({
+    collection: selectShopItems
+})
+
+export default connect(mapStateToProps)(ShopPage)
