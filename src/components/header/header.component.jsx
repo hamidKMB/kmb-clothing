0909777869292b95f8b94f0eebd5b7365b32acd1
hamid Kamyab <hamidkamyab1377@gmail.com>
2021-09-14 20:54:56 +0430

    Convert The HomePage and HeaderComponent Styles From Scss To Styled-Component (Styled-Component Is So Much Cool)

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,51 +1,51 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import CartIcon from "../cart-icon/cart-icon.component";
-import CartDropdown from "../cart-dropdown/cart-dropdown.component";
-import {ReactComponent as Logo} from './crown-logo/4.1 crown.svg'
-import { auth } from "../../firebase/firebase.utils";
-import './header.styles.scss';
-import {createStructuredSelector} from "reselect"
+
+import { createStructuredSelector } from "reselect";
 import { selectCartHidden } from "../../redux/cart/cart-selectors";
 import { selectCurrentUser } from "../../redux/user/user.selectors";
-
 import { connect } from "react-redux";
 
-const Header = ({currentUser, hidden}) => {
-    return (
-      <div className="header">
-        <Link to="/">
-          <Logo className="logo" />
-        </Link>
-        <div className="options">
-          <Link to="/shop" className="option">
-            SHOP
-          </Link>
-          <Link to="shop" className="option">
-            CONTACT
-          </Link>
-          {currentUser ? ( // not null == True
-            <div className="option" onClick={() => auth.signOut()}>
-              SIGN OUT
-            </div> //auth.signOut {fireBase Method}
-          ) : (
-            // null == false
-            <Link className="option" to="signin">
-              SIGN IN
-            </Link>
-          )}
-          <CartIcon />
-        </div>
-        {
-          !hidden && <CartDropdown />
-        }
-      </div>
-    );
-}
+import CartIcon from "../cart-icon/cart-icon.component";
+import CartDropdown from "../cart-dropdown/cart-dropdown.component";
+import { ReactComponent as Logo } from "./crown-logo/4.1 crown.svg";
+
+import { auth } from "../../firebase/firebase.utils";
+
+import {
+  HeaderContainer,
+  LogoContainer,
+  OptionsContainer,
+  OptionDiv,
+  OptionLink,
+} from "./header.styles";
+
+import "./header.styles.scss";
+
+const Header = ({ currentUser, hidden }) => {
+  return (
+    <HeaderContainer>
+      <LogoContainer to="/">
+        <Logo className="logo" />
+      </LogoContainer>
+      <OptionsContainer>
+        <OptionLink to="/shop">SHOP</OptionLink>
+        <OptionLink to="shop">CONTACT</OptionLink>
+        {currentUser ? ( // not null == True
+          <OptionDiv onClick={() => auth.signOut()}>SIGN OUT</OptionDiv> //auth.signOut {fireBase Method}
+        ) : (
+          // null == false
+          <OptionLink to="signin">SIGN IN</OptionLink>
+        )}
+        <CartIcon />
+      </OptionsContainer>
+      {!hidden && <CartDropdown />}
+    </HeaderContainer>
+  );
+};
 
 const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser,
-  hidden: selectCartHidden
+  hidden: selectCartHidden,
   //from rootReducer Select the user and then from setUser Function select the currentUser;
 });
 
